11cc504036f7a21f0ab81562bcaa67608590b09d
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-03 06:21:23 -0800

    Print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+include < stdio.h >
+
+/**
+ *main - do something
+ *Return: return something
+ *
+ *
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
