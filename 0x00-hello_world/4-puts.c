48e922e40d41b2521849e7ec2e8d69bb76bf3494
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-03 06:12:45 -0800

    Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Do something
+ *Return: return something
+ *
+ */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
