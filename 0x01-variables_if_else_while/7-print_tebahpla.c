65306a6f60c750a365426b9f130b0c2b741a76c6
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-04 07:39:48 -0800

    all files

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - asd
+ *
+ *
+ * Return: asd
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
