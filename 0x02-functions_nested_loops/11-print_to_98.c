c9b511dc6bf25a151183561dc25f6602dc006f57
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-07 10:42:54 -0800

    checks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  *print_last_digit - asas
@@ -12,13 +12,13 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 			printf("%d, ", n--);
-		_putchar("%d\n", n);
+		printf("%d\n", n);
 	}
 
 	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
-		_putchar("%d\n", n);
+		printf("%d\n", n);
 	}
 }
