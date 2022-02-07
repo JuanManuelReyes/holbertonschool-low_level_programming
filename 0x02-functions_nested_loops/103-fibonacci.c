c99659125f05bdbf75f0bf26f08811122e894f48
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-07 13:07:13 -0800

    new checks

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - asas
+ *
+ *
+ * Return:asd
+*/
+int main(void)
+{
+long n1 = 0, n2 = 1, sum;
+long total;
+
+	while (1)
+	{
+		sum = n1 + n2;
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total += sum;
+
+		n1 = fib2;
+		n2 = sum;
+	}
+	printf("%ld\n", total);
+
+	return (0);
+}
