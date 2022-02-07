5172c8a4ae254775ae4a50981c4f3ed2d8666cc3
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-07 08:26:41 -0800

    check?

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_sign - asas
+ *
+ *@n: sdf
+ * Return:asd
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+
+}
