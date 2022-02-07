a5f5434af07735accf7b7a77148378b2ac021f90
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-07 04:12:12 -0800

    7/2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - asas
+ *
+ *
+ * Return:asd
+*/
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
+/**
+ *print_alphabet - asdfsdf
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - asas
+ *
+ *
+ * Return:asd
+*/
+void print_alphabet_x10(void);
+int main(void)
+{
+	
+	return (0);
+}
