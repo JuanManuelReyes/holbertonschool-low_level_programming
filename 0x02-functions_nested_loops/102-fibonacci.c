939ebdd2337bc1f9ac95f663ff791f1b6977a62b
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-07 12:58:06 -0800

    new checks

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 	int i;
-	int n1 = 0, n2 = 1, sum;
+	long n1 = 0, n2 = 1, sum;
 
 	for (i = 0; i < 50; i++)
 	{
 		sum = n1 + n2;
-		printf("%d", sum);
+		printf("%ld", sum);
 
 		n1 = n2;
 		n2 = sum;
