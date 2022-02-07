253049bc7aef3099475651c716229ba1bef5738b
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-07 10:25:30 -0800

    checks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,35 @@
 #include "main.h"
 
 /**
- *jack_bauer - asas
+ *times_table - asas
  *
- *@n: sdf
+ *
  * Return:asd
 */
-void jack_bauer(void)
+void times_table(void)
 {
-	
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar('0' + (prod / 10));
+
+			_putchar('0' + (prod % 10));
+
+		}
+		_putchar('\n');
+	}
+
 }
