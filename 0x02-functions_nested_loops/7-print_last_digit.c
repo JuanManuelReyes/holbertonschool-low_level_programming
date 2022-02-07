9ead309727b0c3271a48b093e3108c07aa8a7ea4
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-07 08:48:50 -0800

    check?

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_last_digit - asas
+ *
+ *@n: sdf
+ * Return:asd
+*/
+int print_last_digit(int n)
+{
+	int las-dig = n % 10
+
+	if(las-dig < 0)
+	{
+		las-dig *= -1;
+	}
+	_putchar(las-dig + 'o');
+	return (las-dig);
+
+}
