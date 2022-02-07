295cc20820b509ac6d540c04fc433d8f718c1245
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-07 10:12:44 -0800

    checks

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,5 +8,19 @@
 */
 void jack_bauer(void)
 {
+	int hs, min;
 
+	for (hs = 0; hs <= 23; hs++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+			_putchar('\n');
+
+		}
+	}
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *jack_bauer - asas
+ *
+ *@n: sdf
+ * Return:asd
+*/
+void jack_bauer(void)
+{
+	
+}
