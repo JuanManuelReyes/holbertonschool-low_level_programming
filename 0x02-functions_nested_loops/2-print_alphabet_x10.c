78de150f8a92615266d9b32567f72d6ec3f00e97
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-07 04:37:22 -0800

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,6 +9,21 @@
 void print_alphabet_x10(void);
 int main(void)
 {
-	
+	print_alphabet_x10();
 	return (0);
+}
+/**
+ *print_alphabet_x10 - asdfsdf
+*/
+void print_alphabet_x10(void)
+{
+	int num = 0;
+	char letter;
+
+	while (num++ <= 9)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	}
 }
