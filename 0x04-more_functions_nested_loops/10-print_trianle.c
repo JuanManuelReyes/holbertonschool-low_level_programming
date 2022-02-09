b9875bde824850e211599cf2d0a584e618e9fd57
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-09 08:13:30 -0800

    asd

--- 0x04-more_functions_nested_loops/10-print_trianle.c
+++ 0x04-more_functions_nested_loops/10-print_trianle.c
@@ -8,6 +8,23 @@
 */
 void print_triangle(int size)
 {
-	
+int x, i;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (i= size - x; i> 0; i--)
+				_putchar(' ');
+
+			for (i= 0; i < x; i++)
+				_putchar('#');
+
+			if (x == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}	
 }
 
