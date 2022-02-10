e7f97162efc30f684a17e8a5c667af06f752b07f
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-09 16:34:20 -0800

    asd

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,33 +7,31 @@
  * Return:asd
 */
 void print_number(int n)
-}
+{
 	unsigned int dc, nat, dig;
 	double f = 1;
 
 	nat = n;
 
-
 	if (nat == 0)
 		_putchat('0');
 	else
 	{
-	if (n < 0)
-	{
-		nat = -n;
-		_putchat('-');
-	}
+		if (n < 0)
+		{
+			nat = -n;
+			_putchat('-');
+		}
+		while (f <= nat)
+			f *= 10;
+		dc = f / 10;
 
-	while (f <= nat)
-		f *= 10;
-	dc = f / 10;
-
-	while (dc >= 1)
-	{
-		dig = nat / f;
-		_putchar(dig + '0');
-		nat = (nat - (dc * dig));
-		dc /= 10;
+		while (dc >= 1)
+		{
+			dig = nat / f;
+			_putchar(dig + '0');
+			nat = (nat - (dc * dig));
+			dc /= 10;
+		}
 	}
-}
 }
