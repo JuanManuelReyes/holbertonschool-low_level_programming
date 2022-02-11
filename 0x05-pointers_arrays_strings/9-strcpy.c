492696b889dda95fc677a0c89ace20ab0ccafbd7
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-11 13:35:36 -0800

    asd

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,21 @@
 #include "main.h"
 /**
+ *_strlen - asd
+ *@s: asd
+ *Return: asd
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s)
+		i++;
+
+	return (i);
+}
+
+
+/**
  *_strcpy - asd
  *@dest: asd
  *@src: asd
@@ -7,13 +23,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-	while (src[i])
-	{
+	for (i = 0; i <= _strlen(src); i++)
 		dest[i] = src[i];
-		i++;
-	}
 
 	return (dest);
 
