e73b6c2dd5139824aa0ad07bf1275e9fa36f97d4
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-11 08:45:40 -0800

    asd

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,9 +6,10 @@
  */
 int _strlen(char *s)
 {
-    int i = 0;
-    while(s[i]!='\0')
-    	i++;
+	int i = 0;
 
-    return (i);
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts - asd
+ *@str: asd
+ *
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
