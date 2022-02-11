11fe351ccb54b94fdbb79d21ca7692742977fb3a
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-11 10:57:07 -0800

    asd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
- *print_rev - asd
- *@s: asd
+ *puts_half - asd
+ *@str: asd
  *
  */
 void puts_half(char *str)
 {
 	int pos = 0;
-        int i = 0;
-        int n = 0;
+	int i = 0;
+	int n = 0;
 
-        while (str[pos] != '\0')
-                pos++;
+	while (str[pos] != '\0')
+		pos++;
 
-        if (pos % 2 == 0)
-                n = pos / 2;
+	if (pos % 2 == 0)
+		n = pos / 2;
 
-        else
-                n = (pos + 1) / 2;
+	else
+		n = (pos - 1) / 2;
 
 
-        for (i = n; i < pos; i ++)
-                _putchar(str[i]);
+	for (i = n; i < pos; i++)
+		_putchar(str[i]);
 
-        _putchar('\n');
-
+	_putchar('\n');
 }
