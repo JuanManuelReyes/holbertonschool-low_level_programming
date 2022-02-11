8b7311cd1fb62b659a817f39acffa386cfbdab89
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-11 07:58:45 -0800

    asd

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - asd
+ *@n: asd
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *swap_int - asd
+ *@a: asd
+ *@b: asd
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int swp = *a;
+	*a = *b;
+	*b = swp;
+}
