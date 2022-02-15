fc3553909eab62acb7f2eaeef1e891a96f816188
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-15 15:16:38 -0800

    newThings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*reverse_array- asd
+*@a: asd
+*@n: asd
+**/
+char *rot13(char *)
+{
+	
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,15 @@ char *cap_string(char *string)
 
 	while (string[i])
 	{
-			
+		while (!(string[i] >= 'a' && string[i] <= 'z'))
+			i++;
+
+		if(string[i - 1] == ' ' || string[i - 1] == '\n' || string[i - 1] == '\t' || string[i - 1] == ',' || string[i - 1] == ';' || string[i - 1] == '.' || string[i - 1] == '!' || 
+		string[i - 1] == '?' || string[i - 1] == '"' || string[i - 1] == '(' || string[i - 1] == ')' || string[i - 1] == '{' || string[i - 1] == '}' || i == 0)
+			string[i] -= 32;
+
+		i++;
 	}
+
+	return (string);
 }
