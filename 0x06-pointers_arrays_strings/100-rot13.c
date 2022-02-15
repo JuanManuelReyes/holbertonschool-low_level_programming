b66b4ceb213e623a36a1891eab7b07b3c900bfdb
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-15 15:41:44 -0800

    just2forTheEnd

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,7 +4,21 @@
 *@a: asd
 *@n: asd
 **/
-char *rot13(char *)
+char *rot13(char *encoded)
 {
-	
+	int i = 0, x = 0;
+
+	char base[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char chng[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; encoded[i] != '\0'; i++)
+	{
+		for (x = 0; base[i] != '\0'; x++)
+		{
+			if (encoded[i] == base[x])
+				encoded[i] = chng[x];
+		}
+	}
+
+	return (encoded);	
 }
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*reverse_array- asd
+*@a: asd
+*@n: asd
+**/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	
+}
