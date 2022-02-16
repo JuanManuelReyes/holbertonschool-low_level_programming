7fb242aad94f039d435244cc562b19b245c19e16
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-16 03:42:30 -0800

    puta

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, length = 0;
 
-	while (dest[i++])
+	while (dest[i++] != '\0')
 		length++;
 
 	for (i = 0;i < n && src[i] != '\0'; i++, length++)
