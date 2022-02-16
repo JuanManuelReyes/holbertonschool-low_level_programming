da71d207e9c0462e8148eb9ffbef22ff47111980
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-16 06:29:21 -0800

    a

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,21 +6,39 @@
 **/
 char *rot13(char *encoded)
 {
-int i = 0, x = 0;
+	int i = 0, x = 0;
 
-	char base[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char chng[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char base[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			 'G', 'H', 'I', 'J', 'K', 'L',
+			 'M', 'N', 'O', 'P', 'Q', 'R',
+			 'S', 'T', 'U', 'V', 'W', 'X',
+			 'Y', 'Z', 'a', 'b', 'c', 'd',
+			 'e', 'f', 'g', 'h', 'i', 'j',
+			 'k', 'l', 'm', 'n', 'o', 'p',
+			 'q', 'r', 's', 't', 'u', 'v',
+			 'w', 'x', 'y', 'z'};
 
-	for (i = 0; encoded[i] != '\0'; i++)
+	char chng[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			 'T', 'U', 'V', 'W', 'X', 'Y',
+			 'Z', 'A', 'B', 'C', 'D', 'E',
+			 'F', 'G', 'H', 'I', 'J', 'K',
+			 'L', 'M', 'n', 'o', 'p', 'q',
+			 'r', 's', 't', 'u', 'v', 'w',
+			 'x', 'y', 'z', 'a', 'b', 'c',
+			 'd', 'e', 'f', 'g', 'h', 'i',
+			 'j', 'k', 'l', 'm'};
+
+	while (encoded[i])
 	{
-		for (x = 0; base[i] != '\0'; x++)
+		for (x = 0; x < 52; x++)
 		{
-			if (encoded[i] == base[x])
-			{
-				encoded[i] = chng[x];
-				break;
-			}
+			 if (encoded[i] == base[x])
+                        {
+                                encoded[i] = chng[x];
+                                break;
+                        }
 		}
+		i++;
 	}
 
 	return (encoded);
