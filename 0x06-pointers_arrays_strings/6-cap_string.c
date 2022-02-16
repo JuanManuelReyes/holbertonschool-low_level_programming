6516d1004068366e7449a7ff2678c4c373a4d62f
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-16 05:05:52 -0800

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,9 +13,11 @@ char *cap_string(char *string)
 		while (!(string[i] >= 'a' && string[i] <= 'z'))
 			i++;
 
-		if(string[i - 1] == ' ' || string[i - 1] == '\n' || string[i - 1] == '\t' || string[i - 1] == ',' || string[i - 1] == ';' ||
-		string[i - 1] == '.' || string[i - 1] == '!' || string[i - 1] == '?' || string[i - 1] == '"' || string[i - 1] == '(' ||
-		string[i - 1] == ')' || string[i - 1] == '{' || string[i - 1] == '}' || i == 0)
+		if(string[i - 1] == ' ' || string[i - 1] == '\n' || string[i - 1] == '\t' ||
+		string[i - 1] == ',' || string[i - 1] == ';' || string[i - 1] == '.' ||
+		string[i - 1] == '!' || string[i - 1] == '?' || string[i - 1] == '"' ||
+		string[i - 1] == '(' || string[i - 1] == ')' || string[i - 1] == '{' ||
+		string[i - 1] == '}' || i == 0)
 			string[i] -= 32;
 
 		i++;
