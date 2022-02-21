2152671801d8fdc8935875cb355ac6b1475a536f
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-21 09:08:57 -0800

    Task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,30 @@
 #include "main.h"
 /**
-*_strpbrk- asd
-*@accept: asd
-*@s: asd
+*_strstr- asd
+*@haystack: asd
+*@needle: asd
 *Return: asd
 **/
 char *_strstr(char *haystack, char *needle)
 {
-	
+	int i;
+	int length = 0;
+
+	while (needle[length] != '\0')
+		length++;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != length)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return ('\0');
+
 }
