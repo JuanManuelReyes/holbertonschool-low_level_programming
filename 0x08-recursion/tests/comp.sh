72ec1d9b94d09998a92945d6bdefe66160b91eca
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-23 03:56:52 -0800

    NewProyect

---
+++ 0x08-recursion/tests/comp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gcc ../_putchar.c 0-main.c ../0-puts_recursion.c -o 0-puts_recursion
+gcc ../_putchar.c 1-main.c ../1-print_rev_recursion.c -o 1-print_rev_recursion
+gcc 2-main.c ../ 2-strlen_recursion.c -o  2-strlen_recursion
+gcc 3-main.c ../ 3-factorial.c -o  3-factorial
+gcc 4-main.c ../4-pow_recursion.c -o 4-pow_recursion
+gcc 5-main.c ../5-sqrt_recursion.c -o 5-sqrt_recursion
+gcc  6-main.c ../6-is_prime_number.c -o 6-is_prime_number
+gcc 100-main.c ../100-is_palindrome.c -o 100-is_palindrome
+gcc 101-main.c ../101-wildcmp.c -o 101-wildcmp
