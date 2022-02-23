60c2873126af56dfd75c01ec8e456211841ae85d
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-23 13:03:36 -0800

    BettyTo6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,9 +12,8 @@ int divisible(int num, int divisor)
 		return (0);
 	else if (divisor == num / 2)
 		return (1);
-	
-	return (divisible(num, divisor +1));
 
+	return (divisible(num, divisor + 1));
 }
 
 
@@ -28,7 +27,7 @@ int is_prime_number(int n)
 	int divisor = 2;
 
 	if (n <= 1)
-		return 0;
+		return (0);
 	else if (n >= 2 && n <= 3)
 		return (1);
 
