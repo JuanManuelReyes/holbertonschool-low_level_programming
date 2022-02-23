d85faff199bfe0a0ce5bc64319f1d818597ef66c
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-23 13:00:23 -0800

    Task5&6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,36 @@
 #include "main.h"
 
 /**
+ *_sqrt - asd
+ *@num: asd
+ *@root: asd
+ *Return: asd
+ */
+int _sqrt(int num, int root)
+{
+	if (root * root == num)
+		return (root);
+	else if (root == num / 2)
+		return (-1);
+	else
+		return (_sqrt(num, root + 1));
+}
+
+
+
+/**
  *_sqrt_recursion - asd
  *@n: asd
  *Return: asd
  */
 int _sqrt_recursion(int n)
 {
-	
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	else
+		return (_sqrt(n, root));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,23 @@
 #include "main.h"
+
+/**
+ *divisible - asd
+ *@num: asd
+ *@divisor: asd
+ *Return: asd
+ */
+int divisible(int num, int divisor)
+{
+	if (num % divisor == 0)
+		return (0);
+	else if (divisor == num / 2)
+		return (1);
+	
+	return (divisible(num, divisor +1));
+
+}
+
+
 /**
  *is_prime_number - asd
  *@n: asd
@@ -6,5 +25,12 @@
  */
 int is_prime_number(int n)
 {
+	int divisor = 2;
 
+	if (n <= 1)
+		return 0;
+	else if (n >= 2 && n <= 3)
+		return (1);
+
+	return (divisible(n, divisor));
 }
