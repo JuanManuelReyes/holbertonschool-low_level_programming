b3953cd898cb88fea684f47d9287ba4c85cd4067
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-25 08:10:17 -0800

    BettyTo2

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - asas
+ *@argc: asd
+ *@argv: asd
+ * Return:asd
+*/
+int main(int argc, char *argv[])
+{
+	int num, sum, res;
+
+	return (0);
+}
