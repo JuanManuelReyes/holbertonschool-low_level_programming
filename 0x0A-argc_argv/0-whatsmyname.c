5ed40434dab4af9125ff52bfb2154367794b7fdc
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-25 04:22:53 -0800

    argc&argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - asas
+ *@argc: asd
+ *@argv: asd
+ * Return:asd
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
