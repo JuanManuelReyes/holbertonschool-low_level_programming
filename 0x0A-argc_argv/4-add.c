170fb44975ef3aacc96eaf8fbde532ab0b22421b
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-02-25 08:46:04 -0800

    Task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,21 @@
 */
 int main(int argc, char *argv[])
 {
-	int num, sum, res;
+	int num, sum, res = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (sum = 0; argv[num][sum]; sum++)
+		{
+			if (argv[num][sum] < '0' || argv[num][sum] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		res += atoi(argv[num]);
+	}
+	printf("%d\n", res);
 
 	return (0);
 }
