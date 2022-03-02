dabd1b98e84f28e5155d1e71e771a6a6f8f8175e
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-02 07:11:01 -0800

    Task 2 fixed 100%

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,11 @@ char *str_concat(char *s1, char *s2)
 	char *string;
 	int length_s1 = 0, length_s2 = 0, i, x;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+
+	if (s2 == NULL)
 		s2 = "";
-	}
 
 	while (*(s1 + length_s1))
 		length_s1++;
