00b0bbb5c02f791bd309510e04279d51449cca36
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-02 08:00:37 -0800

    New Changes

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *alloc_grid - asd
+ *@with: asd
+ *@heig: asd
+ *Return: asd
+ **/
+int **alloc_grid(int width, int height)
+{
+	
+}
