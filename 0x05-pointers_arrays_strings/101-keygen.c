aa61cbf08b83d55d544d7da2f51e8a3be586b74d
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-07 13:02:03 -0800

    Sera?

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,24 @@
-sd
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+
+/**
+ *main - asd
+ *Return: asd
+ **/
+int main(void)
+{
+	int n, i = 2772;
+
+	n = rand() % 98;
+
+	while (i != 0)
+	{
+		putchar(n);
+		i -= n;
+		if (i <= 125 && i >= 33)
+			n = i;
+	}
+
+	return (0);
+}
