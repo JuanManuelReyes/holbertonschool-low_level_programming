d11c9dd80f556f67dbbeba69d1d7cf1fe5130fd0
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-08 13:48:25 -0800

    Another Solution maybe

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,38 +11,44 @@
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int name_length, owner_length, i;
-	dog_t *dog;
+	int name_length, owner_length, i = 0, x = 0;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-	dog = malloc(sizeof(dog_t));
+	dog_t *dog;
+	dog = (dog_t *)malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 		return (NULL);
-	for (i = 0; name[i]; i++)
+	
+	while (name[i++])
 		name_length++;
-	name_length++;
+	while (owner[x++])
+		owner_length++;
+
+	name_length += 1;
+	owner_length += 1;
 	dog->name = malloc(name_length * sizeof(char));
+
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < name_length; i++)
+
+	for (i = 0; i <= name_length; i++)
 		dog->name[i] = name[i];
+
 	dog->age = age;
-	for (i = 0; owner[i]; i++)
-		owner_length++;
-	owner_length++;
 	dog->owner = malloc(owner_length * sizeof(char));
+
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < owner_length; i++)
-		dog->owner[i] = owner[i];
+
+	for (x = 0; x <= owner_length; x++)
+		dog->owner[x] = owner[x];
 
 	return (dog);
 }
