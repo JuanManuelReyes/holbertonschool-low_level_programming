bdc28856dd0ff9bba2f305e952600e54610542a6
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-09 07:05:45 -0800

    Else feo

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,5 +9,20 @@
  **/
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	
+	int i;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+
+	return (-1);
 }
