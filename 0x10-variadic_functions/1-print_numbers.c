ba6609b5d0235e25e7b6e987a6316a88820ecf17
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-10 04:58:21 -0800

    CorrectOutPutExtra,

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * sum_them_all - asd
@@ -14,7 +16,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	while (i < n)
 	{
-		printf("%d\n", va_arg(args, int));
+		printf("%d", va_arg(args, int));
 		i++;
+
+		if (separator != NULL)
+			printf("%s", separator);
+
 	}
+	printf("\n");
+
+	va_end(args);
 }
