896964497434b84693c97432abdbc18906200e70
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-10 04:52:55 -0800

    1WithErros

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,5 +7,14 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i = 0;
 
+	va_start(args, n);
+
+	while (i < n)
+	{
+		printf("%d\n", va_arg(args, int));
+		i++;
+	}
 }
