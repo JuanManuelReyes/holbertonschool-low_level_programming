9d06264e101a57797a205463c54582b797b20f88
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-10 03:17:10 -0800

    $commitMsg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - asd
+ * @n: asd
+ * Return: asd
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
---
+++ 0x10-variadic_functions/tests/comp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+gcc 0-main.c ../0-sum_them_all.c -o a
+gcc 1-main.c ../1-print_numbers.c -o b
+gcc 2-main.c ../2-print_strings.c -o c
+gcc 3-main.c ../3-print_all.c -o d
+
