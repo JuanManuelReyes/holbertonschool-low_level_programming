423524ab1e961ee3cf00472386eec64ca8a7916c
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-10 05:57:09 -0800

    headerStruct

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/**
+ * struct print - asd
+ * @type: asd
+ * @func: asd
+ */
+typedef struct print
+{
+	char *type;
+	void (*func)(va_list);
+} print_t;
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
