f3381a0de1ef6e75f1206e2e8c6e87d43b915763
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-10 03:56:02 -0800

    $commitMsg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - asd
@@ -7,5 +8,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int sum = 0, i = 0;
 
+	va_start(args, n);
+
+	while (i < n)
+	{
+		sum = va_arg(args, unisged int) + sum;
+		i++
+	}
+
+	va_end(args);
+
+	return (sum);
 }
