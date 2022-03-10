71f849dc4ca6fbdf1dae62a5c713da811aa2a7ed
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-10 05:55:51 -0800

    Functions_Created

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,47 @@
 #include <stdio.h>
 
 /**
+ * print_c - asd
+ * @c: asd
+ */
+void print_c(va_list c)
+{
+    printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_s - asd
+ * @s: asd
+ */
+void print_s(va_list s)
+{
+    char *str = va_arg(s, char *);
+
+    if (str == NULL)
+        str = "(nil)";
+
+    printf("%s", str);
+}
+
+/**
+ * print_i - asd
+ * @i: asd
+ */
+void print_i(va_list i)
+{
+    printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_f - asd
+ * @f: asd
+ */
+void print_f(va_list f)
+{
+    printf("%f", va_arg(f, double));
+}
+
+/**
  * sum_them_all - asd
  * @n: asd
  * Return: asd
