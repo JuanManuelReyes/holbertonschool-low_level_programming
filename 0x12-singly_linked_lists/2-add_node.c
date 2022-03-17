a8e2b71e7912097216cae0690c425161e548eec6
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-17 07:09:27 -0700

    AgusTest

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *print_list - asd
+ *@head: asd
+ *@str: asd
+ *Return: asd
+ **/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_list;
+
+	if(str == NULL)
+		return (NULL);
+
+	new_list = malloc(sizeof(list_t));
+
+	if (new_list == NULL)
+		return (NULL);
+
+
+
+	
+}
