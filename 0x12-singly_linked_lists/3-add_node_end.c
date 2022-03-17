707b50f8d9b0462d6d854dc9eda1dd723ecc3515
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-17 11:27:56 -0700

    asd

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ * _strlen - asd
+ * @string: asd
+ * Return: asd
+ **/
+unsigned int _strlen(const char *string)
+{
+	unsigned int i = 0;
+
+	while (string[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
  *add_node_end - asd
  *@head: asd
  *@str: asd
@@ -8,5 +23,31 @@
  **/
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_end_node, *last_pos;
 
+	if (str == NULL)
+		return (NULL);
+
+	new_end_node = malloc(sizeof(list_t));
+
+	if (new_end_node == NULL)
+		return (NULL);
+
+	new_end_node->str = strdup(str);
+	new_end_node->len = _strlen(str);
+	new_end_node->next = NULL;
+
+	if (!*head)
+	{
+		*head = new_end_node;
+	}
+	else
+	{
+		last_pos = *head;
+		while (last_pos->next)
+			last_pos = last_pos->next;
+
+		last_pos->next = new_end_node;
+	}
+	return (new_end_node);
 }
