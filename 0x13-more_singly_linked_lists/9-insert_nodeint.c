ca6409feae938f4bfe093a85695462246465a606
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-22 04:33:20 -0700

    Nine

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ *insert_nodeint_at_index - asd
+ *@head: asd
+ *@idx: asd
+ *@n: asd
+ *Return: asd
+ **/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *actual, *next;
+
+	actual = *head;
+
+	next = malloc(sizeof(lisint_t));
+
+	if (next == NULL)
+		return (NUL);
+
+	next->n = n;
+
+	if (idx == 0)
+	{
+		next->next = acual;
+		*head = next;
+		return (*head);
+	}
+
+	while (idx >= 1)
+	{
+		actual = actual->next;
+		idx--;
+
+		if (actual == NULL)
+		{
+			free(next);
+			return (NULL);
+		}
+
+	}
+
+	next->next = actual->next;
+	actual->next = next;
+
+	return (next);
+}
