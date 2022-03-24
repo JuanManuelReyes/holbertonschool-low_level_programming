19ac2e191786959f63bd7718065d61266037cc13
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-24 11:58:01 -0700

    FirstTaskTested

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,19 @@
-#include <main.h>
+#include "main.h"
+
+/**
+ * _strlen - asd
+ * @string: asd
+ * Return: asd
+ **/
+unsigned int _strlen(const char *string)
+{
+	unsigned int i = 0;
+
+	while (string[i] != '\0')
+		i++;
+
+	return (i);
+}
 
 /**
  *binary_to_uint - asd
@@ -6,5 +21,27 @@
  **/
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int mul = 1, num = 0, length;
+	int i = 0;
 
+	if (b == NULL)
+		return (0);
+
+	length = _strlen(b);
+
+	i = length - 1;
+
+	while (i >= 0)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		if (b[i] == '1')
+			num += mul;
+
+		mul *= 2;
+		i--;
+	}
+
+	return (num);
 }
