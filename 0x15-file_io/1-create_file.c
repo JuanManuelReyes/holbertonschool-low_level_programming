0daab379b5647139aba50484e38600529d70dabd
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-03-28 12:53:31 -0700

    asd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,3 +15,40 @@ unsigned int _strlen(const char *string)
 	return (i);
 }
 
+/**
+ *create_file - asd
+ *@filename: asd
+ *text_content: asd
+ *Return: asd
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+	ssize_t length_write;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	/* 0600 = (S_IRUSR  00400 + S_IWUSR  00200) */
+
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(file_descriptor);
+		return (1);
+	}
+
+	length_write = write(file_descriptor, text_content, _strlen(text_content));
+
+	if (length_write == -1)
+	{
+		close(file_descriptor);
+		return (-1);
+	}
+
+	close(file_descriptor);
+	return (1);
+}
--- 0x15-file_io/tests/comp.sh
+++ 0x15-file_io/tests/comp.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c ../0-read_textfile.c -o a
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c ../1-create_file.c -o b
