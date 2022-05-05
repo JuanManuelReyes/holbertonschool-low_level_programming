64b436eee16ea3bb8ccbc392f8ba32204f012f20
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-05 10:22:28 -0700

    asd

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - asd
+ * @h: asd
+ * @idx: asd
+ * @n: asd
+ * Return: asd
+ **/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+}
