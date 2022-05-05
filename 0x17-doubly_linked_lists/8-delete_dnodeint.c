f9a7c780cf10199b9b1c9c910f517bf47a462243
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-05 15:16:02 -0700

    asd

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - asd
+ * @head: asd
+ * @index: asd
+ * Return: asd
+ **/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	
+}
