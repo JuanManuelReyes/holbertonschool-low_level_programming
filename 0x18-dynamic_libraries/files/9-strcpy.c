14c60d5f1bf379ebdb29a62c08334cd60a9b476f
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-09 06:49:22 -0700

    asd

---
+++ 0x18-dynamic_libraries/files/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strcpy - asd
+ *@dest: asd
+ *@src: asd
+ *Return: asd
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+		dest[i] = src[i];
+
+	return (dest);
+
+}
