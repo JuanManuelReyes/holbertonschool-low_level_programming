7089e5fb1c9136f4b65f5c242c0f855ac5634925
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-09 12:32:15 -0700

    asd

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-wget -P /tmp/ https://raw.github.com/JuanManuelReyes/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/lib.so
-export LD_PRELOAD="$PWD/../lib.so"
+echo "int srand(void *p){return (0);}int idx = 0;int rand(void){int list[] = {9, 8, 10, 24, 75, 97};idx++
+;return (list[idx]);}" > /root/final.c
+gcc -fPIC --shared /root/final.c -o /root/lib.so
+export LD_PRELOAD=/root/lib.so
