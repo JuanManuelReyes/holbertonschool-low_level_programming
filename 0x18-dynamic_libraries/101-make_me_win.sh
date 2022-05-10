976bd405b664754e10c0e3e42e5cdd3ca49482b3
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-05-09 19:05:52 -0700

    asd

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
-echo "int srand(void *p){return (0);}int idx = 0;int rand(void){int list[] = {9, 8, 10, 24, 75, 97};idx++
-;return (list[idx]);}" > /root/final.c
-gcc -fPIC --shared /root/final.c -o /root/lib.so
-export LD_PRELOAD=/root/lib.so
+#!/bin/bash
+wget -P /tmp/ https://github.com/JuanManuelReyes/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/lib.so?raw=true
+export LD_PRELOAD=/tmp/lib.so
--- 0x18-dynamic_libraries/final.c
+++ 0x18-dynamic_libraries/final.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
-
-void main (void)
+int srand(void *p)
 {
-	printf("9 8 10 24 75 9");
+	return (0);
+}
+int idx = 0;
+int rand(void)
+{
+	int list[] = {9, 8, 10, 24, 75, 97};
+	idx++;
+	return (list[idx]);
 }
