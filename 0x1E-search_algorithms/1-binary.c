e03debd6cc4c7c373233a4ddb758ed145f3100d4
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-25 16:35:35 -0300

    Task 2

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,7 +21,6 @@ int binary_search(int *array, size_t size, int value)
 	{
         printf("Searching in array: ");
 		print_array(array, right_idx, left_idx);
-        printf("\n");
 
 		mid_idx = (left_idx + right_idx) / 2;
 		if (array[mid_idx] == value)
@@ -49,7 +48,7 @@ void print_array(int *array, size_t right, size_t left)
     for (idx = left; idx <= right; idx++)
     {
         if (idx != right)
-            printf("%d", array[idx]);
+            printf("%d,", array[idx]);
         else
             printf("%d\n", array[idx]);
     }
